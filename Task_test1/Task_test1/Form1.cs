dac389535870298ac7054dfa3c353568be48de81
onepre <910682654@qq.com>
2018-04-08 01:07:27 +0800

    Task状态,Task Wait

---
+++ Task_test1/Task_test1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_test1
+{
+    public partial class Form1 : Form
+    {
+        string str1 = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (str1 == "Begin"||str1=="Finish")
+            {
+                listView1.Items.Add(str1+"\n");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var task1 = new Task(() =>
+            {
+                str1 = "Begin";
+                //listView1.BeginInvoke(new Action(() =>
+                //{
+
+                //    listView1.Items.Add(str1 + "\r\n");
+
+                //}));
+                System.Threading.Thread.Sleep(1000);
+                str1 = "Finish";
+                //listView1.BeginInvoke(new Action(() =>
+                //{
+                //    listView1.Items.Add(str1 + "\r\n");
+
+                //}));
+            });
+            str1 = "Before Start:" + task1.Status.ToString();
+            listView1.Items.Add(str1 + "\r\n");
+            task1.Start();
+            str1 = "After Start:" + task1.Status.ToString();
+            listView1.Items.Add(str1 + "\r\n");
+            str1 = "Before Wait:" + task1.Status.ToString();
+            listView1.Items.Add(str1 + "\r\n");
+            task1.Wait();
+            str1 = "After Wait:" + task1.Status.ToString();
+            listView1.Items.Add(str1 + "\r\n");
+        }
+    }
+}
---
+++ task_test_console/task_test(console)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_test_console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task(() => {
+                Console.WriteLine("Begin");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("Finish");
+            });
+            Console.WriteLine("Before Start:"+task.Status);
+            task.Start();
+            Console.WriteLine("After Start:" + task.Status);
+            Console.WriteLine("Before Wait:" + task.Status);
+            task.Wait();
+            Console.WriteLine("After Wait:" + task.Status);
+            Console.Read();
+        }
+    }
+}
